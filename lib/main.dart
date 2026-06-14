8c5dc020bfb137caad15ccd67dca0315bdc41f29
Hafssa <142430994+hafssa01@users.noreply.github.com>
2026-06-14 20:33:54 +0100

    some fixes

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CineApp',
+      title: 'OmdbApp',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
